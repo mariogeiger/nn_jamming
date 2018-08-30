807ee7f9ede430f1477376d8762d789586b20b2a
Mario Geiger <geiger.mario@gmail.com>
2018-08-30 11:38:32 +0200

    fix

--- transition.py
+++ transition.py
@@ -8,7 +8,6 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--log_dir", type=str, required=True)
-    parser.add_argument("--n_parallel", type=int, default=1)
     parser.add_argument("--depth", type=int)
     parser.add_argument("--args", type=str, default="")
     parser.add_argument("--p", type=int, nargs='+', default=[])
@@ -16,7 +15,7 @@ def main():
 
     args = parser.parse_args()
 
-    command = "gpurun python train.py --compute_hessian False --log_dir {log_dir} --p {{p}} --dim {{h}} --width {{h}} --depth {depth} ".format(
+    command = "gpurun python train.py --log_dir {log_dir} --p {{p}} --dim {{h}} --width {{h}} --depth {depth} ".format(
         log_dir=args.log_dir, depth=args.depth) + args.args
 
     for p in args.p:
@@ -25,15 +24,16 @@ def main():
         hs = [x for x in hs if x <= max_width]
         hs = sorted(hs, reverse=True)
 
-        print("hs = {}".format(hs))
+        print(">>> hs={}".format(hs))
 
         for h in hs:
             cmd = command.format(p=p, h=h)
+            print(">>> " + cmd)
             run = subprocess.Popen(cmd.split())
             run.wait()
 
             run = list(load_dir(args.log_dir))[-1]
-            if run['last']['train'][0] == 0:
+            if run['last']['train'][0] > 0.5 * args.depth * h ** 2:
                 break
 
 
